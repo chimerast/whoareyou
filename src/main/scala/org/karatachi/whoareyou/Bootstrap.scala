960e38e402e551ea66f9558ad54a501690f71edd
Hideyuki TAKEUCHI <webmaster@chimera.st>
2012-01-13 18:58:35 +0900

    リトライするようにした

--- src/main/scala/org/karatachi/whoareyou/Bootstrap.scala
+++ src/main/scala/org/karatachi/whoareyou/Bootstrap.scala
@@ -2,20 +2,30 @@ package org.karatachi.whoareyou
 
 import scala.Array.canBuildFrom
 import scala.io.Source
-
 import org.apache.commons.net.whois.WhoisClient
+import java.net.SocketException
 
 object Bootstrap extends App {
-  for (ipaddress <- Source.fromInputStream(System.in).getLines) {
-    val whois = new WhoisClient()
-    whois.connect("whois.nic.ad.jp")
-    whois
-      .query("NET " + ipaddress + "/e")
-      .split("\n")
-      .filter(_.startsWith("g."))
-      .map(_.substring("g. [Organization]".length).trim)
-      .map((ipaddress, _))
-      .foreach(println)
-    whois.disconnect();
+  for (ipaddress <- Source.fromFile("address.txt").getLines) {
+    var retry = true
+    while (retry) {
+      try {
+        val whois = new WhoisClient()
+        whois.connect("whois.nic.ad.jp")
+        whois
+          .query("NET " + ipaddress + "/e")
+          .split("\n")
+          .filter(_.startsWith("g."))
+          .map(_.substring("g. [Organization]".length).trim)
+          .map((ipaddress, _))
+          .foreach(println)
+        whois.disconnect();
+        retry = false
+      } catch {
+        case e: SocketException =>
+          Thread.sleep(20000)
+          retry = true
+      }
+    }
   }
 }
